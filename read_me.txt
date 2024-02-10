a0e2f4fa4dee0a4e8d33071098b1b7dd26432bc9
Zerak <145105051+Zerak-Hassan@users.noreply.github.com>
2024-02-10 20:53:11 +0000

    init

---
+++ read_me.txt
@@ -0,0 +1,4 @@
+With this program I aim to simulate various dynamics situations to allow for better visulisation of the systems:
+Single and multi degree of freedom systems with both linear and tortional charateristic dimentions. Also with various inputs forces and displacements.
+Rotor dynamics. Balanced and unbalanced shafts and stiffnesses
+Continous systems (Longitudinal, torsional and transverse)
---
+++ single_degree_of_freedom.py
@@ -0,0 +1,21 @@
+import pygame
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+mass=pygame.Rect((SCREEN_WIDTH/2)-50, (SCREEN_HEIGHT/2)-50, 50, 50)
+
+run = True
+while run:
+    screen.fill((255,255,255))
+
+    pygame.draw.rect(screen, (255, 0, 0), mass)#be able to have this rectangle move with some velocity
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=False
+
+    pygame.display.update()
+
+pygame.quit()
