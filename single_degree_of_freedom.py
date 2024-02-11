a46582bc282faf2edde7ed230cc1f8c3b7f233c0
Zerak <145105051+Zerak-Hassan@users.noreply.github.com>
2024-02-11 15:35:32 +0000

    initial dispacement

--- single_degree_of_freedom.py
+++ single_degree_of_freedom.py
@@ -1,14 +1,13 @@
 import pygame
 pygame.init()
 
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))#get the screen resolution somehow
-x0=0
-y0=300
-initial_position_x=(SCREEN_WIDTH/2)-50+x0
-initial_position_y=(SCREEN_HEIGHT/2)-50-y0
+screen = pygame.display.set_mode()
+x0=100
+y0=100
+initial_position_x=pygame.display.get_surface().get_width()/2-25+x0
+initial_position_y=pygame.display.get_surface().get_height()/2-25-y0
 mass=pygame.Rect(initial_position_x, initial_position_y, 50, 50)
+
 
 run = True
 while run:
