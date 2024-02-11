8b615d98f39a824472d27b62661885a0c07b9e6a
Zerak <145105051+Zerak-Hassan@users.noreply.github.com>
2024-02-11 15:12:56 +0000

    Added a way to change the initial position for the mass

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 This is a list of tasks to do:
+-[ ] Look at the ways to position the rect object to see if it can be done in a better way
 -[ ] Impart inital positions and velocities to the mass
 -[ ] Add a spring
--- single_degree_of_freedom.py
+++ single_degree_of_freedom.py
@@ -4,7 +4,11 @@ pygame.init()
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))#get the screen resolution somehow
-mass=pygame.Rect((SCREEN_WIDTH/2)-50, (SCREEN_HEIGHT/2)-50, 50, 50)
+x0=0
+y0=300
+initial_position_x=(SCREEN_WIDTH/2)-50+x0
+initial_position_y=(SCREEN_HEIGHT/2)-50-y0
+mass=pygame.Rect(initial_position_x, initial_position_y, 50, 50)
 
 run = True
 while run:
