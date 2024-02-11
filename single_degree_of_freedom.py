6b2f0bff7803dde6bc52aa248ba68fa6b9f921f0
Zerak <145105051+Zerak-Hassan@users.noreply.github.com>
2024-02-11 16:10:12 +0000

    implemented initial velocity

--- single_degree_of_freedom.py
+++ single_degree_of_freedom.py
@@ -2,19 +2,30 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode()
-x0=100
-y0=100
-initial_position_x=pygame.display.get_surface().get_width()/2-25+x0
-initial_position_y=pygame.display.get_surface().get_height()/2-25-y0
-mass=pygame.Rect(initial_position_x, initial_position_y, 50, 50)
+#initial displacements
+x0=0
+y0=0
+#initial velocities
+xdot0=0
+ydot0=1
 
+size=50#needs to be set based on screen size insted 
+initial_position_x=(pygame.display.get_surface().get_width()-size)/2+x0
+initial_position_y=(pygame.display.get_surface().get_height()-size)/2-y0
+mass=pygame.Rect(initial_position_x, initial_position_y, size, size)
+
+screen.fill((255,255,255))
+pygame.draw.rect(screen, (255, 0, 0), mass)
+pygame.display.update()
 
 run = True
 while run:
+
+    mass.x += xdot0
+    mass.y -= ydot0
     screen.fill((255,255,255))
-
-    pygame.draw.rect(screen, (255, 0, 0), mass)#be able to have this rectangle move with some velocity
-
+    pygame.draw.rect(screen, (255, 0, 0), mass)
+    print(mass.x)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
