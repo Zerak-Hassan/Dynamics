4ee6ac4a6ebd2138a975950929b42275fe511ee6
Zerak <145105051+Zerak-Hassan@users.noreply.github.com>
2024-02-15 12:16:38 +0000

    set up equilibrium and basic spring

--- single_degree_of_freedom.py
+++ single_degree_of_freedom.py
@@ -4,23 +4,33 @@ pygame.init()
 clock=pygame.time.Clock()
 
 screen = pygame.display.set_mode()
-#initial displacements
+#equilibrium position
 x0=0
 y0=0
 #initial velocities
-xdot0=0
-ydot0=1
+xdot0=10
+ydot0=0
 
 #Spring constants
 
 x_spring = 1
 y_spring = 1
 
-size=50#needs to be set based on screen size insted 
-initial_position_x=(pygame.display.get_surface().get_width()-size)/2+x0
-initial_position_y=(pygame.display.get_surface().get_height()-size)/2-y0
+size=50#the size of the mass but should be based on screen size insted
+x_equilibrium = (pygame.display.get_surface().get_width()-size)/2
+y_equilibrium = (pygame.display.get_surface().get_height()-size)/2
+initial_position_x=x_equilibrium+x0
+initial_position_y=y_equilibrium-y0
 mass=pygame.Rect(initial_position_x, initial_position_y, size, size)
-
+#inital position
+screen.fill((255,255,255))
+pygame.draw.rect(screen, (255, 0, 0), mass)
+pygame.display.update()
+#t=1 position
+xdot=xdot0-(mass.x-x_equilibrium)*x_spring
+ydot=ydot0-y_spring*(mass.y-y_equilibrium)
+mass.x += xdot
+mass.y -= ydot
 screen.fill((255,255,255))
 pygame.draw.rect(screen, (255, 0, 0), mass)
 pygame.display.update()
@@ -28,13 +38,15 @@ pygame.display.update()
 run = True
 while run:
 
-    clock.tick(1)
+    clock.tick(60)
 
-    mass.x += xdot0-x_spring*(mass.x-x0)
-    mass.y -= ydot0-y_spring*(mass.y-y0)
+    xdot -= x_spring*(mass.x-initial_position_x)
+    mass.x += xdot
+    mass.y -= ydot0#-y_spring*(mass.y-initial_position_y)
     screen.fill((255,255,255))
     pygame.draw.rect(screen, (255, 0, 0), mass)
-    print(mass.x)
+    print(mass.x,mass.y)
+    print(xdot)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
