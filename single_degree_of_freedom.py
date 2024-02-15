9245592cb3af5bdcde81d26fb093890ab3612c65
Zerak <145105051+Zerak-Hassan@users.noreply.github.com>
2024-02-15 11:36:38 +0000

    init

--- single_degree_of_freedom.py
+++ single_degree_of_freedom.py
@@ -11,6 +11,11 @@ y0=0
 xdot0=0
 ydot0=1
 
+#Spring constants
+
+x_spring = 1
+y_spring = 1
+
 size=50#needs to be set based on screen size insted 
 initial_position_x=(pygame.display.get_surface().get_width()-size)/2+x0
 initial_position_y=(pygame.display.get_surface().get_height()-size)/2-y0
@@ -25,8 +30,8 @@ while run:
 
     clock.tick(1)
 
-    mass.x += xdot0
-    mass.y -= ydot0
+    mass.x += xdot0-x_spring*(mass.x-x0)
+    mass.y -= ydot0-y_spring*(mass.y-y0)
     screen.fill((255,255,255))
     pygame.draw.rect(screen, (255, 0, 0), mass)
     print(mass.x)
